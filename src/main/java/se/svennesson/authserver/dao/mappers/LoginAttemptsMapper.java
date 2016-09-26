d1e9b99a8807c0a761b399eeb4a2600787192ca1
NEXUSGROUP\Marcus.Svensson <marcus.svensson@nexusgroup.com>
2016-09-26 10:12:24 +0200

    Fix some timezone errors

--- src/main/java/se/svennesson/authserver/dao/mappers/LoginAttemptsMapper.java
+++ src/main/java/se/svennesson/authserver/dao/mappers/LoginAttemptsMapper.java
@@ -6,11 +6,18 @@ import se.svennesson.authserver.models.LoginAttempt;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAccessor;
 
 public class LoginAttemptsMapper implements ResultSetMapper<LoginAttempt>{
 
     @Override
     public LoginAttempt map(int i, ResultSet resultSet, StatementContext statementContext) throws SQLException {
-        return new LoginAttempt(resultSet.getLong("id"), resultSet.getLong("user_id"), resultSet.getBoolean("success"), resultSet.getTimestamp("timestampz").toLocalDateTime());
+        return new LoginAttempt(
+                resultSet.getLong("id"),
+                resultSet.getLong("user_id"),
+                resultSet.getBoolean("success"),
+                resultSet.getTimestamp("timestampz").toLocalDateTime().atZone(ZoneId.ofOffset("UTC", ZoneOffset.ofHours(2))).toLocalDateTime());
     }
 }
